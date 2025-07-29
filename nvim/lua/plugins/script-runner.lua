55ad23a28ed7adaaca37852958251e160f3e2adf
Vito Pistelli <vito.pistelli@lightcast.io>
2025-07-29 15:46:41 -0400

    Update script-runner plugin to use GitHub URL instead of local path

--- nvim/lua/plugins/script-runner.lua
+++ nvim/lua/plugins/script-runner.lua
@@ -1,7 +1,5 @@
 return {
-  -- "viligante8/script-runner.nvim",
-  dir = "/Users/vito.pistelli/dev/personal/script-runner.nvim",
-  name = "script-runner.nvim",
+  "viligante8/script-runner.nvim",
   config = function()
     require("script-runner").setup({})
   end,
