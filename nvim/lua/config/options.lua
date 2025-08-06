1d868e293d5d1c6cc41af28473359aadd2866bb4
Vito Pistelli <vito.pistelli@lightcast.io>
2025-08-06 11:00:54 -0400

    disable telescope and neotree

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
-local opt = vim.opt
-
--- Your core custom options (migrated from kickstart)
--- opt.colorcolumn = "120" -- Column guide at 120 characters (your addition)
